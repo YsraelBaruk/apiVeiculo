d8692389496bdc91fd404a5eaf2dc6baf0187539
baruque <israekbaruque992@gmail.com>
2023-08-28 15:08:49 -0300

    tasfera

--- controller/montadora.php
+++ controller/montadora.php
@@ -16,10 +16,40 @@ function get($consulta, $valor=''){
         $montadoras = $montadora->consultar($valor);
         $viewMontadora->exibirMontadoras($montadoras);
     }
-    else{
+    else if(){
         $montadoras = $montadora->consultar();
         $viewMontadora->exibirMontadora($montadoras);
     }
+    else{
+        $codigo_resposta = 404;
+        $erro = [
+            'result'=>false,
+            'erro'  => 'Erro: 404 - Recurso não encontrado'
+        ];
+        require_once 'view/erro404.php';
+    }  
+
+    // if(count($consulta) == 1 && $consulta[0] == ""){
+    //     // $montadora->consultarPorId($valor);
+    //     $montId = $montadora->consultarPorId($consulta[0]);
+    //     $viewMontadora->exibirMontadora($montId);
+    // }                                               
+    // else if(count($consulta) == 1){
+    //     $montadoras = $montadora->consultar($valor);
+    //     $viewMontadora->exibirMontadoras($montadoras);
+    // }
+    // else if(count($consulta) == 2 && $consulta[0] == "modelo"){
+    //     $montadoras = $montadora->consultar();
+    //     $viewMontadora->exibirMontadora($montadoras);
+    // }
+    // else{
+    //     $codigo_resposta = 404;
+    //     $erro = [
+    //         'result'=>false,
+    //         'erro'  => 'Erro: 404 - Recurso não encontrado'
+    //     ];
+    //     require_once 'view/erro404.php';
+    // }  
 }
 
 function post($dados_montadora){
